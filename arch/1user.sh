eafb62b5a3eae8d736a28fef55434fb1ebd05975
jontxux <jonbergerandi@gmail.com>
2025-05-24 18:33:27 +0200

    eliminado directorios de SO que no estamos usando

--- arch/1user.sh
+++
@@ -1,8 +0,0 @@
-read -p "Username: " username
-username=`echo $username | tr '[:upper:]' '[:lower:]'`
-useradd -m -g users -G wheel -s /bin/bash ${username}
-chfn ${username}
-passwd ${username}
-while [[ $? -ne 0 ]]; do
-    passwd ${username}
-done
